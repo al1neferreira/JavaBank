f9929b5025109c2095599d9f87db0e8c48621ec1
Aline Ferreira <alinfst@gmail.com>
2023-01-19 19:31:04 -0300

    Estudos de Java - Orientação a Objetos - Criando uma segunda instância

--- src/CriaConta.java
+++ src/CriaConta.java
@@ -4,5 +4,15 @@ public class CriaConta {
         Conta primeiraConta = new Conta();
         primeiraConta.saldo = 200;
         System.out.println(primeiraConta.saldo);
+
+        primeiraConta.saldo += 100;
+        System.out.println(primeiraConta.saldo);
+
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 50;
+
+        System.out.println("A primeira conta tem um saldo de " + primeiraConta.saldo);
+        System.out.println("A segunda conta tem um saldo de " + segundaConta.saldo);
+
     }
 }
