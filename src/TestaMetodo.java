bedfed69f52d2a40bc2186d3674ae21b3a8f234a
Aline Ferreira <alinfst@gmail.com>
2023-01-20 22:03:15 -0300

    Estudos de Java - Orientação a Objetos - Metodos 1

--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -5,5 +5,10 @@ public class TestaMetodo {
     public static void main(String[] args) {
 
         Conta contaDaAline = new Conta();
+
+        contaDaAline.saldo = 100;
+        contaDaAline.deposita(50);
+
+        System.out.println(contaDaAline.saldo);
     }
 }
